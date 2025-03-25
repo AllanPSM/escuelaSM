810a2af70048aafef179fa96981f33d9e5469fdb
allan <allan@DESKTOP-3VP6S4L>
2025-03-25 11:13:02 +0100

    creacion del proyecto escuela

---
+++ src/test/java/com/formacion/EscuelaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscuelaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
